6c505c91a93240482d30457b300d6e72af5942e7
Marc Felden <marc.felden@me.com>
2016-03-24 18:56:52 +0100

    Initial Commit

---
+++ TDDLaufschriftPruefungsAufgabe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDDLaufschriftPruefungsAufgabe
+//
+//  Created by Marc Felden on 24.03.16.
+//  Copyright © 2016 Timm Kent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
